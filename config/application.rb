21949839697ca1ef1f13f9da422d5598e64f9d3b
shan <2302531309@qq.com>
2024-03-24 16:04:43 +0800

    feat: 添加 session 中间件

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,12 @@ module RailsBackendScaffold
       from: "#{ENV['SITE_NAME']} <#{ENV['EMAIL_FROM']}>",
       reply_to: ENV['REPLY_TO']
     }
+
+    # This also configures session_options for use below
+    config.session_store :cookie_store, key: '_interslice_session'
+
+    # Required for all session management (regardless of session_store)
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
   end
 end
