8e4d2138ce1f77c5c2c01ed4669412b87315e90a
shan <2302531309@qq.com>
2024-04-06 11:25:34 +0800

    fix: 修复重定向跳转到外部域名的错误

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -10,9 +10,9 @@ class Users::OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksCon
 
   def render_data_or_redirect(message, data, user_data = {})
     if message == 'deliverCredentials'
-      redirect_to ENV.fetch('REDIRECT_SUCCESS_URL')
+      redirect_to ENV.fetch('REDIRECT_SUCCESS_URL'), allow_other_host: true
     elsif message == 'authFailure'
-      redirect_to ENV.fetch('REDIRECT_FAIL_URL')
+      redirect_to ENV.fetch('REDIRECT_FAIL_URL'), allow_other_host: true
     end
   end
 end
