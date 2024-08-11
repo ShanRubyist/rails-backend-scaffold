ed4076a8ed69112b26edf01abc8948ae0e41e29b
shan <2302531309@qq.com>
2024-08-11 23:40:54 +0800

    fix: 修复 google oauth 问题

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :google_oauth2, ENV['GOOGLE_KEY'],   ENV['GOOGLE_SECRET'], {
-    redirect_uri: 'https://' + ENV.fetch('HOST') +  '/omniauth/google_oauth2/callback'
+    redirect_uri: 'https://' + ENV.fetch('FAKE_HOST') +  '/omniauth/google_oauth2/callback'
   }
 
   # provider :github, ENV['GITHUB_KEY'],   ENV['GITHUB_SECRET'],   scope: 'email,profile'
