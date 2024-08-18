01651b843e2a1a8eb4c8a2306f8bd8484f86ba4d
shan <2302531309@qq.com>
2024-08-19 00:25:36 +0800

    feat: 更新扣费逻辑

--- app/controllers/concerns/credits_counter.rb
+++ app/controllers/concerns/credits_counter.rb
@@ -15,14 +15,17 @@ module CreditsCounter
   end
 
   def left_credits(user)
-    credits = total_credits(user) - total_used_credits(user)
+    credits = total_credits(user) - total_used_credits(user) + (ENV.fetch('FREEMIUM_CREDITS') { 0 }).to_i
 
     credits = 0 if credits < 0
     return credits
   end
 
   def used_credits(user, usage_klass_name)
-    user.send(usage_klass_name).count
+    # TODO: refactor
+    user.send(usage_klass_name)
+        .where("replicated_calls.data->>'status' = ?", 'succeeded')
+        .sum(:cost_credits)
   end
 
   def usage_classes
--- app/controllers/usage_controller.rb
+++ app/controllers/usage_controller.rb
@@ -1,11 +1,29 @@
 class UsageController < ApplicationController
   before_action :authenticate_user!
-  before_action :check_authorization, only: :create
+
+  include Usage
+  before_action :check_credits
 
   private
 
-  def check_authorization
-    authorize :usage, :create?
+  def check_credits
+    unless credits_enough?
+      render json: {
+        message: 'You do not has enough credits'
+      }.to_json, status: 403
+    end
   end
 
+  def current_cost_credits
+    case params[:model]
+    when nil
+      1
+    when 'black-forest-labs/flux-schnell'
+      1
+    when 'black-forest-labs/flux-dev'
+      10
+    when 'black-forest-labs/flux-pro'
+      20
+    end
+  end
 end
