9c046245053ffd32a4746edf7874da0d2a932d87
shan <2302531309@qq.com>
2024-11-15 20:20:42 +0800

    feat：支持google one tap

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   root "users/sessions#new"
 
   post 'token', to: 'users/omniauth_callbacks#token'
+  post 'google_onetap_token', to: 'users/google_one_tap#token'
 
   post 'stripe_checkout', to: 'payment#stripe_checkout', as: 'stripe_checkout'
   get 'stripe_billing', to: 'payment#stripe_billing', as: 'billing'
