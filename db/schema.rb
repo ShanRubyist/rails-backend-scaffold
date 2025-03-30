6ba0627be5bb25480650951cd32b19f952d1701b
shan <2302531309@qq.com>
2025-03-30 10:51:17 +0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_03_22_000004) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_29_035916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -205,7 +205,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_22_000004) do
     t.string "pricing_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_tools_on_name"
+    t.index ["name"], name: "index_tools_on_name", unique: true
     t.index ["published"], name: "index_tools_on_published"
   end
 
